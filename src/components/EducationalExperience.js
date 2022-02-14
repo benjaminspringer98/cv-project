9bb0298b8e6e97af373cbe62d2f8675c3e04abcd
b.springer <b.springer@stud.uni-goettingen.de>
2022-02-14 10:35:50 +0100

    added components for CV sections

---
+++ src/components/EducationalExperience.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class EducationalExperience extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+      }
+    }
+  
+      render() {
+        return (
+          <div>
+            hello
+          </div>
+        ); 
+      }
+    
+  }
+  
+  export default EducationalExperience;
---
+++ src/components/PracticalExperience.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class PracticalExperience extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+      }
+    }
+  
+      render() {
+        return (
+          <div>
+            hello
+          </div>
+        ); 
+      }
+    
+  }
+  
+  export default PracticalExperience;
