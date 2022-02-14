b6cb6ea20e99473e84f3be7bcd2d333b4ba61d46
b.springer <b.springer@stud.uni-goettingen.de>
2022-02-14 16:55:24 +0100

    educational experience fixing

--- src/components/EducationalExperience.js
+++ src/components/EducationalExperience.js
@@ -1,92 +1,74 @@
 import React, { Component } from "react";
+import uniqid from "uniqid";
 
 class EducationalExperience extends Component {
     constructor() {
       super();
   
-      this.state = {
-        educationalInfo: {
+      this.state = {       
           school: '',
           qualification: '',
           dateFrom: '',
-          dateTo: '',    
-        },  
+          dateTo: '',  
+          id: uniqid(),   
         data: []
       };
     }
 
     handleChange = (e) => {
-      /* if(e.target.id === 'schoolInput') {
-        this.setState({
-          educationalInfo: {
-            school: e.target.value
-          }
-        });
-      }
-      else if(e.target.id === 'qualificationInput') {
-        this.setState({
-          educationalInfo: {
-            qualification: e.target.value
-          }
-        });
-      }
-      else if(e.target.id === 'dateFrom') {
-        this.setState({
-          educationalInfo: {
-            dateFrom: e.target.value
-          }
-        });
-      }
-      else if(e.target.id === 'dateTo') {
-        this.setState({
-          educationalInfo: {
-            dateTo: e.target.value
-          }
-        });
-      } */
-    const id = e.target.id;
-    const value = e.target.value;
-    this.setState({
-      educationalInfo: {
-        [id]: value
-      }
-    });
-
+      const name = e.target.name;
+      const value = e.target.value;
+      this.setState({
+        [name]: value,
+        id: this.state.id
+      });
     };
 
     onSubmit = (e) => {
       e.preventDefault();
+      const educationalInfo = {school: this.state.school, qualification: this.state.qualification, dateFrom: this.state.dateFrom, dateTo: this.state.dateTo, id: this.state.id}
       this.setState({
-        data: this.state.data.concat(this.state.educationalInfo),
-        educationalInfo: {
+          data: this.state.data.concat(educationalInfo),
           school: '',
           qualification: '',
           dateFrom: '',
-          dateTo: ''
-        }          
+          dateTo: '',
+          id: uniqid()       
       });
     };
 
   
       render() {
-        const  { educationalInfo } = this.state;
+        const  { data, school, qualification, dateFrom, dateTo, id } = this.state;
 
         return (
           <div>
             <h2>Educational Experience</h2>
             <form onSubmit={this.onSubmit}>
                 <label htmlFor="schoolInput">School</label>
-                <input onChange={this.handleChange} value={educationalInfo.school} type="text" id="school"/>
+                <input onChange={this.handleChange} value={school} type="text" id="school" name="school"/>
                 <label htmlFor="qualificationInput">Qualification</label>
-                <input onChange={this.handleChange} value={educationalInfo.qualification} type="text" id="qualification"/>
+                <input onChange={this.handleChange} value={qualification} type="text" id="qualification" name="qualification"/>
                 <label htmlFor="dateFrom">From</label>
-                <input onChange={this.handleChange} value={educationalInfo.dateFrom} type="date" id="dateFrom"/>
+                <input onChange={this.handleChange} value={dateFrom} type="date" id="dateFrom" name="dateFrom"/>
                 <label htmlFor="dateTo">To</label>
-                <input onChange={this.handleChange} value={educationalInfo.dateTo} type="date" id="dateTo"/>
+                <input onChange={this.handleChange} value={dateTo} type="date" id="dateTo" name="dateTo"/>
                 <button type="submit">
                   Add
                 </button>        
             </form>
+           <ul>
+            {data.map((education) => {
+              {console.log(education)}
+              return <li key={id}><p>{education.school}</p>
+                                   <p>{education.qualification}</p>
+                                   <p>{education.dateFrom}</p>
+                                   <p>{education.dateTo}</p>
+                                   <p>{education.id}</p>
+
+                     </li>;      
+            })}
+          </ul>
           </div>
         ); 
       }
